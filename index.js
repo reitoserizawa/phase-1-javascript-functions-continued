28284fc4b194edf29cb80615b5e828ea3ea2fee5
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-05 12:06:02 -0400

    commit tree d8c2e91382ab3ca9bec2baf1624cb4457a516ef6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (firstParameter = 'roller-skate') {
+    return `This Saturday, I want to ${firstParameter}!`
+}
+
+const mondayWork = function (doWork = 'go to the office') {
+    return `This Monday, I will ${doWork}.`
+}
+
+function wrapAdjective (whateverString = '*') {
+    return function (whatever='special') {
+        return `You are ${whateverString}${whatever}${whateverString}!`
+    }
+}
+
+// // It should take as parameter a String that will be used to create visual flair
+// You may call the parameter whatever you like, but its default value should be "*"
+// Call example: const encouragingPromptFunction = wrapAdjective("!!!")
